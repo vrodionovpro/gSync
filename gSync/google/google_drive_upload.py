1bf8a265447e89ef0fcbd179bd5cef96ffcf7c08
vrodionovpro <vrodionov.pro@gmail.com>
2025-03-11 00:48:49 +0400

    1

--- gSync/google/google_drive_upload.py
+++ gSync/google/google_drive_upload.py
@@ -2,31 +2,30 @@ import os
 import sys
 import google.auth
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload, MediaIoBaseUpload
-import io
+from googleapiclient.http import MediaFileUpload
 import time
 
 def upload_file(service_account_path, file_path, file_name, folder_id):
-    # Аутентификация
+    # Аутентификация с использованием сервисного аккаунта
     credentials, _ = google.auth.load_credentials_from_file(service_account_path)
     drive_service = build('drive', 'v3', credentials=credentials)
 
     try:
-        # Проверка существования файла
+        # Проверка существования файла в указанной папке
         query = f"name='{file_name}' and '{folder_id}' in parents"
         results = drive_service.files().list(q=query, fields="files(id, name)").execute()
         if results.get('files', []):
             print(f"already exists")
             return False
 
-        # Настройки для resumable upload
-        CHUNK_SIZE = 256 * 1024 * 1024  # 256 MB chunks
+        # Настройки для resumable upload с чанками по 256 MB
+        CHUNK_SIZE = 256 * 1024 * 1024  # 256 MB
         file_size = os.path.getsize(file_path)
         media = MediaFileUpload(file_path,
                               chunksize=CHUNK_SIZE,
                               resumable=True)
 
-        # Инициализация загрузки
+        # Инициализация загрузки файла
         request = drive_service.files().create(
             body={'name': file_name, 'parents': [folder_id]},
             media_body=media,
@@ -41,7 +40,7 @@ def upload_file(service_account_path, file_path, file_name, folder_id):
             if status:
                 uploaded_size = status.resumable_progress
                 current_time = time.time()
-                if current_time - last_progress_time >= 10:  # Обновление каждые 10 секунд
+                if current_time - last_progress_time >= 10:  # Обновление прогресса каждые 10 секунд
                     progress = int((uploaded_size / file_size) * 100)
                     print(f"PROGRESS:{progress}%", flush=True)  # Отправка прогресса
                     last_progress_time = current_time
@@ -55,6 +54,7 @@ def upload_file(service_account_path, file_path, file_name, folder_id):
         return False
 
 if __name__ == "__main__":
+    # Проверка корректности аргументов командной строки
     if len(sys.argv) != 5:
         print("Usage: python google_drive_upload.py <service_account_path> <file_path> <file_name> <folder_id>")
         sys.exit(1)
